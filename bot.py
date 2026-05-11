d99a9ac08816c1a0bccaaa12c864a87865262ba1
7770hassan-crypto <7770hassan@gmail.com>
2026-05-11 21:31:15 +0300

    Update تشغيل البوتbot.py
    تجربة تشغيل البوت في Railway

--- bot.py
+++ bot.py
@@ -4,3 +4,15 @@ from telegram import Bot
 from config import TOKEN, CHAT_ID
 from strategy import signal
 from stocks import stocks
+import asyncio
+from telegram import Bot
+from config import TOKEN, CHAT_ID
+
+bot = Bot(token=TOKEN)
+
+async def main():
+    while True:
+        bot.send_message(chat_id=CHAT_ID, text="البوت شغال الآن ✅")
+        await asyncio.sleep(60)
+
+asyncio.run(main())
