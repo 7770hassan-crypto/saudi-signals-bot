514c3b965b9aea4b674f4f07f31ca66181490320
7770hassan-crypto <7770hassan@gmail.com>
2026-05-11 21:34:59 +0300

    تشغيل البوت
    تجربة تشغيل البوت في Railway

--- bot.py
+++ bot.py
@@ -1,10 +1,4 @@
 import asyncio
-import yfinance as yf
-from telegram import Bot
-from config import TOKEN, CHAT_ID
-from strategy import signal
-from stocks import stocks
-import asyncio
 from telegram import Bot
 from config import TOKEN, CHAT_ID
 
@@ -12,7 +6,10 @@ bot = Bot(token=TOKEN)
 
 async def main():
     while True:
-        bot.send_message(chat_id=CHAT_ID, text="البوت شغال الآن ✅")
+        await bot.send_message(
+            chat_id=CHAT_ID,
+            text="البوت شغال الآن ✅"
+        )
         await asyncio.sleep(60)
 
 asyncio.run(main())
